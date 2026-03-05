84e13dda38360c612483f33eec801211a6dc52e0
furuyaakihiko <akhihikofuruya@gmail.com>
2026-03-05 20:03:56 +0900

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-use App\Http\Controllers\TaskController;
+
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,12 +17,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 Route::get('/test', function () {
     return 'テスト成功';
 });
 
-Route::get('/tasks', function () {
-    return 'tasks OK';
-});
->>>>>>> 87376cd (Add TaskController)
+Route::get('/tasks', [TaskController::class, 'index']);
+
 require __DIR__.'/auth.php';
